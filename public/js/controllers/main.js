a832c22f2b606e67f4fb25a6660500a08e40adab
Rodrigo S <r.sanabriadc@gmail.com>
2014-01-28 09:23:30 -0600

    Carpeta de Models

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -4,7 +4,7 @@ angular.module('appController', [])
         
         Apps.get()
             .success(function(){
-                
+                $scope.p = "HOLA";
                 
             });
  
